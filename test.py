8879e2f9cf16a39eab0320bfe824d2f65dc4b0fa
Tomas Tombakas <tombakas@gmail.com>
2019-02-02 15:40:44 +0100

    Exam frequency count

--- test.py
+++ test.py
@@ -20,13 +20,18 @@ def main():
 
     data = read_file(input_file).strip().split("\n")
 
-    students = parse_studens(data)
+    students, exam_count = parse_studens(data)
     params = parse_header(data[0])
 
     for key, item in students.items():
         print(item, "x", key)
 
+    for exam, frequency in sorted(list(exam_count.items()), key=lambda x: x[1]):
+        print("{:>3}: {:>3}".format(exam, frequency))
+
     print("Exams:", params["exams_tot"])
+    print("Students:", params["students"])
+    print("Days:", params["days"])
     print("Students:", params["students"])
     print("Total slots:", int(params["slots"]) * int(params["days"]))
 
@@ -48,7 +53,8 @@ def parse_header(header):
 def parse_studens(data):
     THRESHOLD = 7
 
-    students = defaultdict(int)
+    student_sets = defaultdict(int)
+    exam_count = defaultdict(int)
 
     exam_list = [item.split(" ")[1:] for item in data[1:-1]]
     exam_list.sort(key=len, reverse=True)
@@ -56,18 +62,21 @@ def parse_studens(data):
     for i, exams in enumerate(exam_list):
         a = set(exams)
 
+        for exam in exams:
+            exam_count[exam] += 1
+
         found = False
-        for key, value in students.items():
+        for key, value in student_sets.items():
             b = set(key.split(" "))
             if (len(a) - len(a.intersection(b))) <= THRESHOLD:
-                students[key] += 1
+                student_sets[key] += 1
                 found = True
                 break
 
         if not found:
-            students[" ".join(exams)] += 1
+            student_sets[" ".join(exams)] += 1
 
-    return students
+    return student_sets, exam_count
 
 
 if __name__ == "__main__":
